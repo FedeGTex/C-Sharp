23ccfca8acb3220059801dd3b4943e01d153114b
alumno <alumno@utnfra.com.ar>
2019-09-10 22:09:11 -0300

    sexto

--- Ejercicios/Clase_3/Ejercicio_20/Ejercicio_20/Ejercicio_20/Program.cs
+++ Ejercicios/Clase_3/Ejercicio_20/Ejercicio_20/Ejercicio_20/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,41 +6,33 @@ using System.Threading.Tasks;
 
 namespace Ejercicio_20
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //Dolar d1 = new Dolar(10, 1);
-            //Dolar d2 = new Dolar(10, 1);
-            //Dolar d3 = new Dolar(20, 1);
-            //Euro e1 = new Euro(30);
-            //Euro e2 = new Euro(40);
-            //Euro e3 = new Euro(40);
-            //Peso p1 = new Peso(50);
-            //Peso p2 = new Peso(60);
-            //Peso p3 = new Peso(60);
+      Dolar d1 = new Dolar(10, 1);
+      Dolar d2 = new Dolar(10, 1);
+      Dolar d3 = new Dolar(20, 1);
+      Euro e1 = new Euro(30);
+      Euro e2 = new Euro(40);
+      Euro e3 = new Euro(40);
+      Peso p1 = new Peso(50);
+      Peso p2 = new Peso(60);
+      Peso p3 = new Peso(60);
 
-            //Console.WriteLine("d1: {0} d2: {1} d3: {2}", d1.GetCantidad(), d2.GetCantidad(), d3.GetCantidad());
-            //Console.WriteLine("e1: {0} e2: {1} e3: {2}", e1.GetCantidad(), e2.GetCantidad(), e3.GetCantidad());
-            //Console.WriteLine("e1: {0} e2: {1} e3: {2}", p1.GetCantidad(), p2.GetCantidad(), p3.GetCantidad());
-            //Console.WriteLine("d1 = d2?: {0}", d1 == d2);
-            //Console.WriteLine("d1 = d3?: {0}", d1 == d3);
-            //Console.WriteLine("d1 != d3?: {0}", d1 != d3);
-            ////Console.WriteLine("d1 + d2 = {0}", d1 + d2);
-            //Console.WriteLine("e1 a dolar: {0}", ((Dolar)e1).GetCantidad());
-            //Console.WriteLine("d1 a euro: {0}", ((Euro)d1).GetCantidad());
-            //Console.WriteLine("e1 a pesos: {0}", ((Peso)e1).GetCantidad());
+      Console.WriteLine("d1: {0} d2: {1} d3: {2}", d1.GetCantidad(), d2.GetCantidad(), d3.GetCantidad());
+      Console.WriteLine("e1: {0} e2: {1} e3: {2}", e1.GetCantidad(), e2.GetCantidad(), e3.GetCantidad());
+      Console.WriteLine("e1: {0} e2: {1} e3: {2}", p1.GetCantidad(), p2.GetCantidad(), p3.GetCantidad());
+
+      Console.WriteLine("e1 a dolar: {0}", ((Dolar)e1).GetCantidad());
+      Console.WriteLine("d1 a euro: {0}", ((Euro)d1).GetCantidad());
+      Console.WriteLine("e1 a pesos: {0}", ((Peso)e1).GetCantidad());
 
 
 
-            //Dolar dolar = 25;
-            //Peso peso = (Peso)dolar;
-            //Euro euro = (Euro)dolar;
-            //Console.WriteLine("{0}   {1}    {2} ", peso.GetCantidad(), dolar.GetCantidad(), euro.GetCantidad());
-            //Console.ReadKey();
 
-            Console.ReadKey();
+      Console.ReadKey();
 
-        }
     }
+  }
 }
